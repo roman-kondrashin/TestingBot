ea24b1bdcb7e577fd1dcab620733d087a358323a
Roman Kondrashin <roman.kondrashin@lamoda.ru>
2016-06-29 21:00:32 +0300

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! TestingBot

--- src/Application/TestingBot.php
+++ src/Application/TestingBot.php
@@ -8,21 +8,35 @@ use Storage\Db\Mysql;
 
 class TestingBot
 {
-    const TOKEN = '221381282:AAHBNtrIFlNGgCB62Fu2Iq0gpUc-nR7_M9A';
+    //const TOKEN = '221381282:AAHBNtrIFlNGgCB62Fu2Iq0gpUc-nR7_M9A';
+    const TOKEN = '227815068:AAHPqrZo7YXh93NxXwOSy80UP3acSPTVPs0';
     const API_URL = 'https://api.telegram.org/bot' . self::TOKEN . '/';
 
     const TEST_TYPE_SIMPLE = 0;
     const TEST_TYPE_COMPLEX = 1;
 
+    const MESSAGE_GET_VIDEO = 'Смотреть видео';
+
     const TEST_TYPE_SIMPLE_TEXT = 'Простой';
     const TEST_TYPE_COMPLEX_TEXT = 'Продвинутый';
 
-    const MESSAGE_HELLO_TEXT = 'Добрый день!
-Вас приветствует тест основы программирования!
-Видео: https://www.youtube.com/watch?v=SW_UCzFO7X0
+    const MESSAGE_START_AGAIN = 'Пройти тест еще раз';
+
+    const MESSAGE_HELLO1_TEXT = 'Добрый день!
+Вас приветствует тренинг и тест по основам программирования!
+Для прохождения теста ознакомтесь с видео.
 ';
-    const MESSAGE_HELLO_QUESTION = 'Выберите уровень сложности:';
-    const MESSAGE_HELLO_KEYBOARD = [[self::TEST_TYPE_SIMPLE_TEXT, self::TEST_TYPE_COMPLEX_TEXT]];
+    const MESSAGE_HELLO1_KEYBOARD = [[self::MESSAGE_GET_VIDEO]];
+
+    const MESSAGE_HELLO2_TEXT = 'https://www.youtube.com/watch?v=SW_UCzFO7X0';
+    const MESSAGE_HELLO2_QUESTION = 'Выберите уровень сложности:';
+    const MESSAGE_HELLO2_KEYBOARD = [[self::TEST_TYPE_SIMPLE_TEXT, self::TEST_TYPE_COMPLEX_TEXT]];
+
+    const MESSAGE_RESULT_TEXT = 'Спасибо!
+Вы прошли тест на %2.2f из %d возможных баллов.
+Тест считается сданным успешно если вы набрали более 80%% с первого раза.
+';
+    const MESSAGE_RESULT_KEYBOARD = [[self::MESSAGE_START_AGAIN]];
 
     /** @var Config */
     private $config;
@@ -34,54 +48,118 @@ class TestingBot
     private $questions = [
         self::TEST_TYPE_SIMPLE => [
             1 => [
-                'text' => 'Какое число зашифровано в двоичном коде "111"?',
+                'text' => 'Вопрос 1 из 5.',
+                'answers' => ['Квадрат', 'Круг', 'Треугольник', 'Прямоугольник', 'Зигзаг'],
+                'right_answer' => true,
+                'results' => [
+                    1 => 0,
+                ],
+            ],
+            2 => [
+                'text' => 'Вопрос 2 из 5. Какое число зашифровано в двоичном коде "111"?',
                 'answers' => ['111', '1', '7', '3'],
                 'right_answer' => '7',
                 'wrong_answer' => 'https://youtu.be/SW_UCzFO7X0?t=500',
+                'results' => [
+                    1 => 1,
+                    2 => 0.5,
+                    3 => 0.35,
+                    4 => 0.25,
+                ],
             ],
-            2 => [
-                'text' => 'Как в двоичном коде выглядит число 50?',
+            3 => [
+                'text' => 'Вопрос 3 из 5. Как в двоичном коде выглядит число 50?',
                 'answers' => ['10010001', '00020202', '50', '00110010'],
                 'right_answer' => '00110010',
                 'wrong_answer' => 'https://youtu.be/SW_UCzFO7X0?t=746',
+                'results' => [
+                    1 => 1,
+                    2 => 0.5,
+                    3 => 0.35,
+                    4 => 0.25,
+                ],
             ],
-            3 => [
-                'text' => 'Что означают цифры "72" "73" "33" в ASCII?',
+            4 => [
+                'text' => 'Вопрос 4 из 5. Что означают цифры "72" "73" "33" в ASCII?',
                 'answers' => ['HI!', 'ASD', 'QWE', 'YES'],
                 'right_answer' => 'HI!',
                 'wrong_answer' => 'https://youtu.be/SW_UCzFO7X0?t=849',
+                'results' => [
+                    1 => 1,
+                    2 => 0.5,
+                    3 => 0.35,
+                    4 => 0.25,
+                ],
             ],
-            4 => [
-                'text' => 'Какая последовательность чисел используется для RGB?',
+            5 => [
+                'text' => 'Вопрос 5 из 5. Какая последовательность чисел используется для RGB?',
                 'answers' => ['33 16 14', '72 73 33', '22 43 32', '33 22 11'],
                 'right_answer' => '72 73 33',
                 'wrong_answer' => 'https://youtu.be/SW_UCzFO7X0?t=946',
+                'results' => [
+                    1 => 1,
+                    2 => 0.5,
+                    3 => 0.35,
+                    4 => 0.25,
+                ],
             ],
         ],
         self::TEST_TYPE_COMPLEX => [
             1 => [
-                'text' => 'Какое число будет в двоичном коде "01100111"?',
+                'text' => 'Вопрос 1 из 5.',
+                'answers' => ['Квадрат', 'Круг', 'Треугольник', 'Прямоугольник', 'Зигзаг'],
+                'right_answer' => true,
+                'results' => [
+                    1 => 0,
+                ],
+            ],
+            2 => [
+                'text' => 'Вопрос 2 из 5. Какое число будет в двоичном коде "01100111"?',
                 'answers' => ['5', '122', '103', '111'],
                 'right_answer' => '103',
                 'wrong_answer' => 'https://youtu.be/SW_UCzFO7X0?t=500',
+                'results' => [
+                    1 => 1,
+                    2 => 0.5,
+                    3 => 0.35,
+                    4 => 0.25,
+                ],
             ],
-            2 => [
-                'text' => 'Как в двоичном коде выглядит число 300?',
+            3 => [
+                'text' => 'Вопрос 3 из 5. Как в двоичном коде выглядит число 300?',
                 'answers' => ['100100010', '01020202', '100101100', '100110010'],
                 'right_answer' => '100101100',
                 'wrong_answer' => 'https://youtu.be/SW_UCzFO7X0?t=746',
+                'results' => [
+                    1 => 3,
+                    2 => 2,
+                    3 => 1,
+                    4 => 0.5,
+                ],
             ],
-            3 => [
-                'text' => 'Как будет обозначено HELP в ASCII?',
+            4 => [
+                'text' => 'Вопрос 4 из 5. Как будет обозначено HELP в ASCII?',
                 'answers' => ['80 73 89 65', '001101110', '72 69 76 80', '02132123'],
                 'right_answer' => '72 69 76 80',
                 'wrong_answer' => 'https://youtu.be/SW_UCzFO7X0?t=810',
+                'results' => [
+                    1 => 1,
+                    2 => 0.5,
+                    3 => 0.35,
+                    4 => 0.25,
+                ],
             ],
-            4 => [
-                'text' => 'Сколько шагов нужно, чтобы найти запись в 4 миллиардах записей?',
+            5 => [
+                'text' => 'Вопрос 5 из 5. Сколько шагов нужно, чтобы найти запись в 4 миллиардах записей?',
                 'answers' => ['64', '32', '16', '128'],
                 'right_answer' => '32',
                 'wrong_answer' => 'https://youtu.be/SW_UCzFO7X0?t=1373',
+                'results' => [
+                    1 => 3,
+                    2 => 2,
+                    3 => 1,
+                    4 => 0.5,
+                ],
             ],
         ],
     ];
@@ -115,10 +193,15 @@ class TestingBot
         $this->saveMessage($message);
         $number = $this->getCurrentQuestionNumber();
 
+        // again
+        if ($text == self::MESSAGE_START_AGAIN) {
+            $this->clearResults();
+            $text = '/start';
+        }
+
         // hello
         if (strpos($text, '/start') === 0) {
-            $this->sendHelloMessage();
-            $this->sendHelloQuestion();
+            $this->sendHello1Message();
         }
         // test level
         elseif ($number == 0 && $text != self::TEST_TYPE_SIMPLE_TEXT && $text != self::TEST_TYPE_COMPLEX_TEXT) {
@@ -127,7 +210,11 @@ class TestingBot
             $this->saveAnswer($message);
 
             $test_type = $this->getTestType($message);
-            if ($text == self::TEST_TYPE_SIMPLE_TEXT || $text == self::TEST_TYPE_COMPLEX_TEXT) {
+            if ($text == self::MESSAGE_GET_VIDEO) {
+                $this->sendHelloMessage();
+                $this->sendHelloQuestion();
+                exit();
+            } elseif ($text == self::TEST_TYPE_SIMPLE_TEXT || $text == self::TEST_TYPE_COMPLEX_TEXT) {
                 $outMessage = new OutMessage();
                 $outMessage->setChatId($this->chatId);
                 $outMessage->setText($this->getQuestion($test_type, $number + 1));
@@ -140,18 +227,26 @@ class TestingBot
         // in test
         } elseif ($number > 0) {
             $test_type = $this->getTestType($message);
-            if ($number < count($this->questions[$test_type])) {
+            if ($number <= count($this->questions[$test_type])) {
                 if ($this->checkAnswersArray($test_type, $number, $text)) {
                     // get right answer
                     $try_number = $this->getTryNumber($test_type, $number);
                     $correct = $this->getRightAnswer($test_type, $number);
-                    if ($correct == $text) {
+                    if ($correct === true || $correct == $text) {
                         // final
                         $this->saveAnswer($message, $number, $test_type, $try_number + 1, 1);
                         $outMessage = new OutMessage();
-                        $outMessage->setChatId($this->chatId);
-                        $outMessage->setText($this->getQuestion($test_type, $number + 1));
-                        $outMessage->setKeyboard($this->getAnswerKeyboard($test_type, $number + 1));
+                        // last question
+                        if ($number == count($this->questions[$test_type])) {
+                            $result = $this->calculateResult($test_type);
+                            $outMessage->setChatId($this->chatId);
+                            $outMessage->setText(sprintf(self::MESSAGE_RESULT_TEXT, $result->user, $result->max));
+                            $outMessage->setKeyboard(self::MESSAGE_RESULT_KEYBOARD);
+                        } else {
+                            $outMessage->setChatId($this->chatId);
+                            $outMessage->setText($this->getQuestion($test_type, $number + 1));
+                            $outMessage->setKeyboard($this->getAnswerKeyboard($test_type, $number + 1));
+                        }
                     } else {
                         $this->saveAnswer($message, $number, $test_type, $try_number + 1, 0);
                         $outMessage = new OutMessage();
@@ -166,15 +261,45 @@ class TestingBot
                 }
             // out of test
             } else {
-
+                $outMessage = new OutMessage();
+                $outMessage->setChatId($this->chatId);
+                $outMessage->setText('Done');
             }
         }
+    }
+
+    private function clearResults()
+    {
+        $this->dbh->query('DELETE FROM `message` WHERE `chat_id` = ' . $this->chatId);
+        $this->dbh->query('DELETE FROM `answer` WHERE `chat_id` = ' . $this->chatId);
+    }
+
+    private function calculateResult($test_type)
+    {
+        $result = new \stdClass();
+        $result->user = 0;
+        $result->max = 0;
+
+        $data = $this->dbh->fetch('SELECT `question_id`, MAX(`try_number`) AS `try_number` FROM `answer` WHERE `chat_id` = ' . $this->chatId . ' AND `test_type` = ' . $test_type . ' AND `question_id` != 0 GROUP BY `question_id`;');
+        foreach ($data as $answer) {
+            $results = $this->getResults($test_type, $answer->question_id);
+            $try_number = $answer->try_number;
+            $maxTry = max(array_keys($results));
+            if ($try_number > $maxTry) {
+                $result->user += $results[$maxTry];
+            } else {
+                $result->user += $results[$try_number];
+            }
+            $result->max += $results[1];
+        }
+
+        return $result;
     }
 
     private function getTryNumber($test_type, $number)
     {
         $data = $this->dbh->fetchOne('SELECT `try_number` FROM `answer` WHERE `chat_id` = ' . $this->chatId . ' AND `question_id` = ' . $number . ' AND `test_type` = ' . $test_type . ' ORDER BY `update_id` DESC LIMIT 1;');
-        return $data instanceof \stdClass ? $data->try_number - 1 : (int)$data;
+        return $data instanceof \stdClass ? $data->try_number : (int)$data;
     }
 
     private function checkAnswersArray($test_type, $number, $answer)
@@ -193,10 +318,15 @@ class TestingBot
                 $i++;
             }
 
-            $answer[$i][] = $answer;
+            $result[$i][] = $answer;
         }
 
         return $result;
+    }
+
+    private function getResults($test_type, $number)
+    {
+        return $this->questions[$test_type][$number]['results'];
     }
 
     private function getWrongAnswer($test_type, $number)
@@ -285,6 +415,16 @@ SQL;
         $this->sendMessage($message);
     }
 
+    private function sendHello1Message()
+    {
+        $outMessage = new OutMessage();
+        $outMessage->setChatId($this->chatId);
+        $outMessage->setText(self::MESSAGE_HELLO1_TEXT);
+        $outMessage->setKeyboard(self::MESSAGE_HELLO1_KEYBOARD);
+
+        $this->sendMessage($outMessage);
+    }
+
     private function sendHelloMessage()
     {
         $this->sendMessage($this->getHelloMessage());
@@ -313,7 +453,7 @@ SQL;
     {
         $message = new OutMessage();
         $message->setChatId($this->chatId);
-        $message->setText(self::MESSAGE_HELLO_TEXT);
+        $message->setText(self::MESSAGE_HELLO2_TEXT);
 
         return $message;
     }
@@ -322,8 +462,8 @@ SQL;
     {
         $message = new OutMessage();
         $message->setChatId($this->chatId);
-        $message->setText(self::MESSAGE_HELLO_QUESTION);
-        $message->setKeyboard(self::MESSAGE_HELLO_KEYBOARD);
+        $message->setText(self::MESSAGE_HELLO2_QUESTION);
+        $message->setKeyboard(self::MESSAGE_HELLO2_KEYBOARD);
 
         return $message;
     }
